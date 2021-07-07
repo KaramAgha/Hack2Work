34643ebdf2e9deaf9509b015782beb02e4c7693c
Lewis Jones <lewis1997@live.com>
2021-07-07 13:29:34 +0100

    feat(*): update

--- src/script.js
+++ src/script.js
@@ -1,40 +1,14 @@
-function display() {
-    var resultOne;
-    var resultTwo;
-
-    var ele = document.getElementsByName('q-1');          
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked) {
-            resultOne = ele[i].value;
-        }
-    }
-
-    var elem = document.getElementsByName('q-2');          
-    for(i = 0; i < elem.length; i++) {
-        if(elem[i].checked) {
-            resultTwo = elem[i].value;
-        }
-    }
-
-    if (resultOne && resultTwo === 'answer-1') {
-        document.getElementById('result').innerText = "Accountant because you like maths";
-    } else if (resultOne === 'answer-1' && resultTwo === 'answer-2') {
-        document.getElementById('result').innerText = "Marketing";
-    } else if (resultOne === 'answer-1' && resultTwo === 'answer-3') {
-        document.getElementById('result').innerText = "Pilot \n we don't think this is a good job for you";
-    } else {
-        document.getElementById('result').innerText = "Developer";
-    }
-
-    const results = [resultOne, resultTwo];
-    console.log(results);
-
-    return results;
+function updateTextInput(val) {
+    document.getElementById('miles').innerText=val+" miles";
 }
 
-function getResults() {
+function displaySuggestions() {
     const postcode = document.getElementById('postcode').value;
-    if (postcode == 'M4 4AQ') {
-        
+    let distance = document.getElementById('distance').value;
+    if (postcode === "M4 4AQ" && distance < 10) {
+        document.getElementById('outcome').innerText = "In your area, there are 1,500 jobs in marketing.";
+    } else if (postcode === "M4 4AQ" && distance >= 10) {
+        document.getElementById('outcome').innerText = "In your area, there are 4,500 jobs in marketing.";
     }
+    
 }
