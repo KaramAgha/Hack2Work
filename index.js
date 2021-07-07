62268eb3e34350f81e0673f9c69b8b121f0e491a
Karam Agha <ak703@live.com>
2021-07-07 16:56:14 +0100

    remove index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express(); 
-// serves files from the root directory
-app.use(express.static('govuk-prototype-kit-9.14.0/'));
-app.listen(8080, function () {    
-  console.log('Listening to traffic at http://localhost:8080');  
-});
