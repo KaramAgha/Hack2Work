42d1bb4ded003e67cae533a47a04408e3c817d27
Karam Agha <ak703@live.com>
2021-07-07 16:06:22 +0100

    point index.js to prototype

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express(); 
 // serves files from the root directory
-app.use(express.static('./src/'));
+app.use(express.static('./govuk-prototype-kit-9.14.0/'));
 app.listen(8080, function () {    
   console.log('Listening at http://localhost:8080');  
 });
